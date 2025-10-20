c66e9201644ee137a4292b824469b9c8a5302ba7
COCO <bulletflip22@gmail.com>
2025-10-21 04:46:56 +0700

    dockerfile

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['bubblyxyz.com'], // Add your image domains here
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during build
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+  },
+  output: 'standalone', // Use standalone output for better performance in production
+  
 };
 
 export default nextConfig;
